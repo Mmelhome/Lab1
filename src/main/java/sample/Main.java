51292636c267e9a666a2296d448cea8dba37088c
Mmelhome <myelhome@yandex.ru>
2020-04-06 01:19:02 +0300

    second release

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,21 +5,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        /*Dot dot1 = new Dot(0.00, 0.00);
-        Dot dot2 = new Dot(0.00, 3.00);
-        Dot dot3 = new Dot(4.00, 3.00);
 
-        ArrayList<Dot> dots = new ArrayList<Dot>();
-        dots.add(dot1);
-        dots.add(dot2);
-        dots.add(dot3);
-
-        Sf
-
-        Triangle triangle = new Triangle(dots);
-        System.out.println(triangle.getPerimeter());
-        System.out.println(triangle.getSquare());
-
-*/
     }
 }
